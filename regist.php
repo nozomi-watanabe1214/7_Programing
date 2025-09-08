fc8e98756d88268b3aef05fcdff8ba240365ff35
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-09-08 22:56:12 +0900

    html導入のみ記載

---
+++ regist.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>アカウント登録画面</title>
+        <link rel = "stylesheet" type = "text/css" href = "style.css">
+    </head>
