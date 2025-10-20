17dbb231eed61ae21eceafeee0f85a95b59e7430
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-10-20 22:34:56 +0900

    パスワードハッシュ化成功(isset()関数で変数定義)

--- regist_complete.php
+++ regist_complete.php
@@ -1,18 +1,16 @@
 <?php
 mb_internal_encoding("utf8");
 
-if(isset($_POST['password'])){
-    $password = password_hash($_POST['password'],
-                              PASSWORD_DEFAULT);
-} 
-
 $pdo = new PDO("mysql:dbname=registration;host=localhost;","root","");
+
+if(isset($_POST['password'])){
+    $password_after = password_hash($_POST['password'],
+                              PASSWORD_DEFAULT);
+}
 
 $pdo -> exec("insert into account(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority)
 values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$_POST['password']."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."');");
 
-$_POST['password']->execute(array(
-      ':password' =>  password_hash($$_POST['password'],PASSWORD_DEFAULT)));
 ?>
 
 
