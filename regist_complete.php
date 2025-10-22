2c521eedacad064c066940f53670c6b0075243dc
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-10-22 22:28:12 +0900

    try catchにてエラーメッセージ表示・「登録完了できました」も出てしまう

--- regist_complete.php
+++ regist_complete.php
@@ -1,20 +1,24 @@
 <?php
 mb_internal_encoding("utf8");
 
-$pdo = new PDO("mysql:dbname=registration;host=localhost;","root","");
-
-$password_after = password_hash($_POST['password'],PASSWORD_DEFAULT);
-
-if(isset($_POST['password'])){
+try{
+    $pdo = new PDO("mysql:dbname=registratio;host=localhost;","root","");
+    
+    $password_after = password_hash($_POST['password'],PASSWORD_DEFAULT);
+    
+    if(isset($_POST['password'])){
     $password_after = password_hash($_POST['password'],
                               PASSWORD_DEFAULT);
-}
-
-$registered_time = date('Y-m-d H:i:s');
-
-$pdo -> exec("insert into account(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,registered_time)
+    
+    }
+    
+    $registered_time = date('Y-m-d H:i:s');
+    
+    $pdo -> exec("insert into account(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,registered_time)
 values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$password_after."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."','".$registered_time."');");
-
+} catch(PDOException $e){
+    echo "エラーが発生したためアカウント登録できません。";
+}
 ?>
 
 
