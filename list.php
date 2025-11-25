6853fef91eb165fb3d231f14f321a7542f0d3a26
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-11-25 22:52:30 +0900

    アカウント一覧画面用　link.php作成

---
+++ list.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>アカウント登録画面</title>
+        <link rel = "stylesheet" type = "text/css" href = "style2.css">
+    </head>
+    
+    <body>
+        <header></header>
+        
+    <h1>アカウント一覧画面</h1>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <footer></footer>
