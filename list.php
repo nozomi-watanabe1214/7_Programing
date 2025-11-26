ac657ba7eef2c30de0bdd88f321c7156acd27ebf
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-11-26 21:47:29 +0900

    アカウント一覧画面テーブル　項目部分作成

--- list.php
+++ list.php
@@ -3,13 +3,38 @@
     <head>
         <meta charset="UTF-8">
         <title>アカウント登録画面</title>
-        <link rel = "stylesheet" type = "text/css" href = "style2.css">
+        <link rel = "stylesheet" type = "text/css" href = "style5.css">
     </head>
     
     <body>
         <header></header>
         
     <h1>アカウント一覧画面</h1>
+        
+        
+        if(mysqli_num_rows($result) > 0){
+        $table = '
+        <table>
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>名前（性）</th>
+            <th>名前（名）</th>
+            <th>カナ（性）</th>
+            <th>カナ（名）</th>
+            <th>メールアドレス</th>
+            <th>性別</th>
+            <th>アカウント権限</th>
+            <th>削除フラグ</th>
+            <th>登録日時</th>
+            <th>更新日時</th>
+            <th>操作</th>
+        </tr>
+        </thead>';
+            
+        
+        }
+            </table>
         
         
         
