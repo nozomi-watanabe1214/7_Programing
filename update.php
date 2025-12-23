720bb02748da774c56501696d3b180706afe4b40
渡辺のぞみ <shogo55.kk4@gmail.com>
2025-12-23 23:06:27 +0900

    アカウント更新画面　パスワード・性別・都道府県・アカウント権限以外完成

---
+++ update.php
@@ -0,0 +1,172 @@
+<?php
+
+mb_internal_encoding("UTF8");
+
+$dsn = "mysql:dbname=registration;host=localhost;charset=utf8";
+
+$user = "root";
+$password = "";
+$data = [];
+
+$dbh = new PDO($dsn, $user, $password);
+
+try{
+    $pdo = new PDO($dsn, $user, $password);
+} catch(PDOException $e){
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$sql = "SELECT family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority, delete_flag, registered_time, update_time FROM account where id = {$_POST['id']}";
+
+$stmt = $pdo->query($sql);
+
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>アカウント登録画面</title>
+        <link rel = "stylesheet" type = "text/css" href = "style9.css">
+    </head>
+    
+    <body>
+        <header></header>
+        
+    <h1>アカウント登録画面</h1>
+    <form method = "post" action = "update_confirm.php">
+             
+        <div>
+            <label>名前(性)</label>
+            <input type="text" class ="text" size = "35" name= "family_name" value="<?php echo ($row['family_name'])?>" maxlength = "10" pattern = "^[ぁ-ん一-龠ー]*$" required>
+        </div> 
+        
+        <div>
+            <label>名前(名)</label>
+            <input type = "text" class ="text" size = "35" name = "last_name" value="<?php echo ($row['last_name'])?>" maxlength = "10" pattern = "^[ぁ-ん一-龠ー]*$" required>
+        </div>
+        
+        <div>
+            <label>カナ(性)</label>
+            <input type = "text" class = "text"
+                   size = "35" name = "family_name_kana" value="<?php echo ($row['family_name_kana'])?>" maxlength = "10" pattern = "^[ァ-ンヴー]*$" required>
+        </div>
+        
+        <div>
+            <label>カナ(名)</label>
+            <input type = "text" class = "text"
+                   size = "35" name = "last_name_kana" value="<?php echo ($row['last_name_kana'])?>" maxlength = "10" pattern = "^[ァ-ンヴー]*$"　required>
+        </div>
+        
+        <div>
+            <label>メールアドレス</label>
+            <input type = "text" class = "text"
+                   size = "35" name = "mail" value="<?php echo ($row['mail'])?>" maxlength = "100" pattern = "^[0-9a-zA-Z\-\@\.]*$" required>   
+        </div>
+        
+        <div>
+            <label>パスワード</label>
+            <input type = "password" class = "text"
+                   size = "35" name = "password" maxlength = "10" pattern = "^[a-zA-Z0-9]*$" required>
+        </div>
+        
+        <div>
+            <label>性別</label>
+            <input type = "radio" name = "gender" value = "0">男
+            <input type = "radio" name = "gender" value = "1" required>女
+        </div>
+        
+        <div>
+            <label>郵便番号</label>
+            <input type = "text" class = "text"
+                   size = "35" name = "postal_code" value="<?php echo ($row['postal_code'])?>"pattern="^[0-9]{7}$" required>
+        </div>
+        
+        <div>
+            <label>住所（都道府県）</label>
+            <select name="prefecture" required>
+                <option value="" disabled selected></option>
+                <option value="北海道">北海道</option>
+                <option value="青森県">青森県</option>
+                <option value="岩手県">岩手県</option>
+                <option value="宮城県">宮城県</option>
+                <option value="秋田県">秋田県</option>
+                <option value="山形県">山形県</option>
+                <option value="福島県">福島県</option>
+                <option value="茨城県">茨城県</option>
+                <option value="栃木県">栃木県</option>
+                <option value="群馬県">群馬県</option>
+                <option value="埼玉県">埼玉県</option>
+                <option value="千葉県">千葉県</option>
+                <option value="東京都">東京都</option>
+                <option value="神奈川県">神奈川県</option>
+                <option value="新潟県">新潟県</option>
+                <option value="富山県">富山県</option>
+                <option value="石川県">石川県</option>
+                <option value="福井県">福井県</option>
+                <option value="山梨県">山梨県</option>
+                <option value="長野県">長野県</option>
+                <option value="岐阜県">岐阜県</option>
+                <option value="静岡県">静岡県</option>
+                <option value="愛知県">愛知県</option>
+                <option value="三重県">三重県</option>
+                <option value="滋賀県">滋賀県</option>
+                <option value="京都府">京都府</option>
+                <option value="大阪府">大阪府</option>
+                <option value="兵庫県">兵庫県</option>
+                <option value="奈良県">奈良県</option>
+                <option value="和歌山県">和歌山県</option>
+                <option value="鳥取県">鳥取県</option>
+                <option value="島根県">島根県</option>
+                <option value="岡山県">岡山県</option>
+                <option value="広島県">広島県</option>
+                <option value="山口県">山口県</option>
+                <option value="徳島県">徳島県</option>
+                <option value="香川県">香川県</option>
+                <option value="愛媛県">愛媛県</option>
+                <option value="高知県">高知県</option>
+                <option value="福岡県">福岡県</option>
+                <option value="佐賀県">佐賀県</option>
+                <option value="長崎県">長崎県</option>
+                <option value="熊本県">熊本県</option>
+                <option value="大分県">大分県</option>
+                <option value="宮崎県">宮崎県</option>
+                <option value="鹿児島県">鹿児島県</option>
+                <option value="沖縄県">沖縄県</option>
+            </select>
+        </div>
+        
+        <div>
+            <label>住所（市区町村）</label>
+            <input type = "text" class = "text"
+                   size = "35" name = "address_1" value="<?php echo ($row['address_1'])?>"maxlength = "10" pattern = "^[ぁ-んァ-ンヴ一-龠ー0-9０-９\-－\s　]*$" required>
+        </div>
+        
+        <div>
+            <label>住所（番地）</label>
+            <input type = "text" class = "text"
+                   size = "35" name = "address_2" value="<?php echo ($row['address_2'])?>"maxlength = "100" pattern = "^[ぁ-んァ-ンヴ一-龠ー0-9０-９\-－\s　]*$" required>
+        </div>
+        
+        <div>
+            <label>アカウント権限</label>
+            <select name = "authority" required>
+                <option value = "0">一般</option>
+                <option value = "1">管理者</option>
+            </select>
+        </div> 
+        
+        <div>
+            <input type = "submit" class = "submit" value = "確認する">
+        </div>
+    
+            </form>
+        
+
+        
+        <footer></footer>
+    
+    </body>
+</html>
