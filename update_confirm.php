e056ad0068957b36c81ea9ae2597cc6b2ed714f4
渡辺のぞみ <shogo55.kk4@gmail.com>
2026-01-13 23:27:01 +0900

    アカウント更新確認画面　データベース接続コード追加

--- update_confirm.php
+++ update_confirm.php
@@ -1,3 +1,29 @@
+<?php
+
+mb_internal_encoding("UTF8");
+
+$dsn = "mysql:dbname=registration;host=localhost;charset=utf8";
+
+$user = "root";
+$password = "";
+$data = [];
+
+$dbh = new PDO($dsn, $user, $password);
+
+try{
+    $pdo = new PDO($dsn, $user, $password);
+} catch(PDOException $e){
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$sql = "SELECT family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority, delete_flag, registered_time, update_time FROM account where id = {$_POST['id']}";
+
+$stmt = $pdo->query($sql);
+
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
 <!docutype HTML>
 <html lang = "ja">
 
@@ -96,6 +122,8 @@
         <input type = "submit" class = "button1" value = "前に戻る" formaction="javascript:history.back()">
     
         <input type = "submit" class = "button2" value = "更新する" formaction="update_complete.php" method="post">
+        <input type = "hidden" value = "<?php 
+                                        echo $_POST['id']; ?>" name = "id">
         <input type = "hidden" value = "<?php 
                                         echo $_POST['family_name']; ?>" name = "family_name">
         <input type = "hidden" value = "<?php 
