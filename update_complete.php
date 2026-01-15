a0225dc3e1eb172f006c60d7d60f1365f82f8661
渡辺のぞみ <shogo55.kk4@gmail.com>
2026-01-15 21:50:22 +0900

    アカウント更新完了画面　パスワードハッシュ化（UPDATEの際に入力値と別変数にするので表記変更）

--- update_complete.php
+++ update_complete.php
@@ -33,15 +33,15 @@ $update_time = date('Y-m-d H:i:s');
 //$pdo -> exec("UPDATE account set(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,update_time)
 //values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$password_after."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."','".$update_time."');");
     
-$update = $pdo->prepare("UPDATE account SET family_name=:family_name, last_name=:last_name, family_name_kana=:family_name_kana, last_name_kana=:last_name_kana, mail=:mail, password=:password, gender=:gender, postal_code=:postal_code, prefecture=:prefecture, address_1=:address_1, address_2=:address_2, authority=:authority where id = {$_POST['id']}");
+$update = $pdo->prepare("UPDATE account SET family_name=:family_name, last_name=:last_name, family_name_kana=:family_name_kana, last_name_kana=:last_name_kana, mail=:mail, password='$password_after', gender=:gender, postal_code=:postal_code, prefecture=:prefecture, address_1=:address_1, address_2=:address_2, authority=:authority where id = {$_POST['id']}");
 //上記にupdate_time含めず
+//passwordはハッシュ化して別変数なので:なしの'$〇〇'にしないと文字列として認識されてエラーになる
     
 $update->bindValue(':family_name', $family_name);
 $update->bindValue(':last_name', $last_name);
 $update->bindValue(':family_name_kana', $family_name_kana);
 $update->bindValue(':last_name_kana', $last_name_kana);
 $update->bindValue(':mail', $mail);
-$update->bindValue(':password', $password);
 $update->bindValue(':gender', $gender);
 $update->bindValue(':postal_code', $postal_code);
 $update->bindValue(':prefecture', $prefecture);
