ab1b0fe4f3297717336d3964c7174eaa3f4703ca
渡辺のぞみ <shogo55.kk4@gmail.com>
2026-03-24 23:12:26 +0900

    アカウント一覧画面　性別・権限ともに3つめの選択作成し、全件表示を目指す

--- list.php
+++ list.php
@@ -16,9 +16,9 @@ $dbh = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION
 $search = isset($_POST['search']) ? trim($_POST['search']) : '';
 
 if (isset($_POST["search"])){
-     if (isset($_POST["search_family_name"]) && empty($_POST["search_last_name"])&& 
-        empty($_POST["search_family_name_kana"]) && empty($_POST["search_last_name_kana"]) && 
-        empty($_POST["search_mail"]) && 
+     if (isset($_POST["search_family_name"]) && isset($_POST["search_last_name"])&& 
+        isset($_POST["search_family_name_kana"]) && isset($_POST["search_last_name_kana"]) && 
+        isset($_POST["search_mail"]) && 
         isset($_POST["search_gender"]) && isset($_POST["search_authority"])){
         $search_family_name = $_POST["search_family_name"];
         $search_last_name = '';
@@ -27,60 +27,32 @@ if (isset($_POST["search"])){
         $search_authority = $_POST["search_authority"];
     }
     
-    if (isset($_POST["search_family_name"]) && isset($_POST["search_last_name"])&& 
-        empty($_POST["search_family_name_kana"]) && empty($_POST["search_last_name_kana"]) && 
-        empty($_POST["search_mail"]) && isset($_POST["search_gender"]) && isset($_POST["search_authority"])){
-        $search_family_name = $_POST["search_family_name"];
-        $search_last_name = $_POST["search_last_name"];
-        $search_family_name_kana = '';
-        $search_gender = $_POST["search_gender"];
-        $search_authority = $_POST["search_authority"];
-    }
-    
-     if (isset($_POST["search_family_name"]) && isset($_POST["search_last_name"])&& 
-        isset($_POST["search_family_name_kana"]) && empty($_POST["search_last_name_kana"]) && 
-        empty($_POST["search_mail"]) && isset($_POST["search_gender"]) && isset($_POST["search_authority"])){
-        $search_family_name = $_POST["search_family_name"];
-        $search_last_name = $_POST["search_last_name"];
-        $search_family_name_kana = $_POST["search_family_name_kana"];
-        $search_gender = $_POST["search_gender"];
-        $search_authority = $_POST["search_authority"];
-    }
-    
-      if (isset($_POST["search_family_name"]) && isset($_POST["search_last_name"])&& 
-        isset($_POST["search_family_name_kana"]) && isset($_POST["search_last_name_kana"]) && 
-        empty($_POST["search_mail"]) && isset($_POST["search_gender"]) && isset($_POST["search_authority"])){
-        $search_family_name = $_POST["search_family_name"];
-        $search_last_name = $_POST["search_last_name"];
-        $search_family_name_kana = $_POST["search_family_name_kana"];
-        $search_gender = $_POST["search_gender"];
-        $search_authority = $_POST["search_authority"];
-    }
-    
-      if (isset($_POST["search_family_name"]) && isset($_POST["search_last_name"])&& 
-        isset($_POST["search_family_name_kana"]) && isset($_POST["search_last_name_kana"]) && 
-        isset($_POST["search_mail"]) && isset($_POST["search_gender"]) && isset($_POST["search_authority"])){
-        $search_family_name = $_POST["search_family_name"];
-        $search_last_name = $_POST["search_last_name"];
-        $search_family_name_kana = $_POST["search_family_name_kana"];
-        $search_last_name_kana = $_POST["search_last_name_kana"];
-        $search_mail = $_POST["search_mail"];
-        $search_gender = $_POST["search_gender"];
-        $search_authority = $_POST["search_authority"];
-    }
-    
-    $sql = "SELECT * FROM account WHERE family_name like '%".$_POST["search_family_name"]."%' and last_name like '%".$_POST["search_last_name"]."%' and family_name_kana like '%".$_POST["search_family_name_kana"]."%' and last_name_kana like '%".$_POST["search_last_name_kana"]."%' and mail like '%".$_POST["search_mail"]."%' and gender like '%".$_POST["search_gender"]."%' and authority like '".$_POST["search_authority"]."' ";
+    $sql = "SELECT * FROM account WHERE family_name like '%".$_POST["search_family_name"]."%' and last_name like '%".$_POST["search_last_name"]."%' and family_name_kana like '%".$_POST["search_family_name_kana"]."%' and last_name_kana like '%".$_POST["search_last_name_kana"]."%' and mail like '%".$_POST["search_mail"]."%' and gender like '".$_POST["search_gender"]."' and authority like '".$_POST["search_authority"]."' ";
     
     $rec = $dbh->prepare($sql);
     $rec->execute();
     $rec_list = $rec->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-else{
-    $sql = 'SELECT id, family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority, delete_flag, registered_time, update_time FROM account ORDER BY id DESC';
-    $rec = $dbh -> query($sql);
-    $rec_list = $rec -> fetchAll(PDO::FETCH_ASSOC);
 }
+    
+
+elseif(empty($_POST["search"])) {
+    if (empty($_POST["search_family_name"]) && empty($_POST["search_last_name"])&& 
+        empty($_POST["search_family_name_kana"]) && empty($_POST["search_last_name_kana"]) && 
+        empty($_POST["search_mail"]) && 
+        empty($_POST["search_gender"]) && empty($_POST["search_authority"])){
+        $search_family_name = '';
+        $search_last_name = '';
+        $search_family_name_kana = '';
+        $search_gender = '';
+        $search_authority = '';
+    }
+    
+    $sqlall = 'SELECT id, family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority, delete_flag, registered_time, update_time FROM account ORDER BY id DESC';
+         
+         $rec = $dbh -> prepare($sqlall); 
+         $rec_list = $rec->fetchAll(PDO::FETCH_ASSOC);
+}
+
 
 $dbh=null;
 
@@ -150,6 +122,7 @@ $dbh=null;
             <td>
                 <input type = "radio" name = "search_gender" value = "0" checked>男
                 <input type = "radio" name = "search_gender" value = "1">女 
+                <input type = "radio" name = "search_gender" value = "">両方 
             </td>
         </tr>  
         
@@ -161,6 +134,7 @@ $dbh=null;
                             ?>>一般
                     </option>
                     <option value="1">管理者</option>
+                    <option value=""></option>
                 </select>
             </td>
             
